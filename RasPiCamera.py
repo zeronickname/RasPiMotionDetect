0531bdeaa4ca5e5c84959a231e6551ea2caa221d
zeronickname <maemo@gman.me.uk>
2013-07-15 20:54:25 +0100

    get interval time from ini file

--- RasPiCamera.py
+++ RasPiCamera.py
@@ -14,6 +14,12 @@ password = config.get('DEFAULT','password')
 username = config.get('DEFAULT','username')
 album_name = config.get('DEFAULT','album_name')
 loop_hrs = config.get('DEFAULT','hrs_to_loop')
+interval = config.get('DEFAULT','interval')
+
+# it takes 5-6 seconds to actually take a picture. Compensate for that
+if (interval >6 ):
+    interval -= 6
+
 
 start_time = datetime.datetime.now().time().hour 
 cur_time = datetime.datetime.now().time().hour
@@ -34,6 +40,6 @@ while (cur_time - start_time < loop_hrs):
     call(["raspistill -rot 180 -w 2048 -o " + filename ], shell=True)
     photo = picasa.InsertPhotoSimple(album_url,'New Photo','',filename,content_type='image/jpeg')
     cur_time = datetime.datetime.now().time().hour
-    time.sleep(10)
+    time.sleep(interval)
 
 

a8fdafad3f91b7e509df32ead9a45087b8844613
zeronickname <maemo@gman.me.uk>
2013-07-20 14:33:15 +0100

    set default logging level to WARNING

--- RasPiCamera.py
+++ RasPiCamera.py
@@ -140,10 +140,10 @@ def loglvl_setup():
     parser.add_option('-l', '--logging-level', help='Logging level')
     parser.add_option('-f', '--logging-file', help='Logging file name')
     (options, args) = parser.parse_args()
-    logging_level = LOGGING_LEVELS.get(options.logging_level, logging.NOTSET)
+    logging_level = LOGGING_LEVELS.get(options.logging_level, logging.WARNING)
     logging.basicConfig(level=logging_level, filename=options.logging_file,
-                      format='%(asctime)s %(levelname)s: %(message)s',
-                      datefmt='%Y-%m-%d %H:%M:%S')
+                  format='%(asctime)s %(levelname)s: %(message)s',
+                  datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def main():
